46b4e93285204f67a6c3a53324573f1b10ed5abd
George Hill <georgeophill@outlook.com>
2022-11-15 12:27:16 +0000

    add task model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'tasks/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
 end
